83522b81678beb7373c91ef68291e45466c5c24d
Manish Kumar <manishprajapati.cs1@gmail.com>
2025-04-12 16:32:59 +0530

    update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
     setError('');
     setLoading(true);  
     try {
-      const res = await axios.get(`http://localhost:5000/weather?city=${city}`);
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API}/weather?city=${city}`);
       setWeather(res.data);
       setCity('');
     } catch (err) {
